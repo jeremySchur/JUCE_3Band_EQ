8476fd8680631816a9e5d35f280f89e23d7ed512
Jeremy Schur <jeremy.schur@telosalliance.com>
2025-06-29 16:02:18 -0600

    Added saving of pluging state

--- 3Band_EQ/Source/PluginProcessor.cpp
+++ 3Band_EQ/Source/PluginProcessor.cpp
@@ -184,12 +184,20 @@ void _3Band_EQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void _3Band_EQAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid())
+    {
+        apvts.replaceState(tree);
+        updateFilters();
+    }
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
